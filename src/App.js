e5576708bd04a6e663281c31e103af59f5a294e7
Akash Sharma <akashsharma.zak@gmail.com>
2021-11-24 18:07:15 +0530

    Search functionality implemented

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Form from './components/Form'
 import Display from './components/Display'
 import personService from './services/persons'
+import Search from './components/Search'
 
 const App = () => {
   const [ persons, setPersons ] = useState([])
@@ -9,10 +10,11 @@ const App = () => {
     personService.getAll()
     .then(rtnPsn => setPersons(rtnPsn))
   }, [])
-
+  
   return (
     <div>
       <Form persons={persons} setPersons={setPersons} />
+      <Search persons={persons} setPersons={setPersons} />
       <Display persons={persons} />
     </div>
   )
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import personService from '../services/persons'
 
-const Search = () => {
+const Search = ({ persons, setPersons }) => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
+    const searchHandler = event => {
+        event.preventDefault()
+        if(name!=='' && email!==''){
+            const searchWord = new RegExp(name, 'i')
+            const selName = persons.filter(person => {
+                if(person.name.search(searchWord) !== -1)
+                return person
+
+                return false
+            })
+            if(selName.length > 0){
+                const finalPerson = selName.find(person => person.email === email)
+                persons.length = 0
+            setPersons(persons.concat(finalPerson))
+            }
+            else{
+                alert(`No person's email exists with this name, try again!`)
+            }
+            setName('')
+        setEmail('')
+        }
+        else if(name!=='' && email===''){
+            const searchWord = new RegExp(name, 'i')
+            const newPersons = persons.filter(person => {
+                if(person.name.search(searchWord) !== -1)
+                return person
+
+                return false
+            })
+            setPersons(newPersons)
+            setName('')
+        }
+        else if(name==='' && email!==''){
+            const selPerson = persons.find(person => person.email===email)
+            persons.length = 0
+            setPersons(persons.concat(selPerson))
+            setEmail('')
+        }
+        else{
+            personService.getAll()
+            .then(rtnPsn => setPersons(rtnPsn))            
+        }
+    }
+
     return (
-    <form>
-
-    </form>
+        <form onSubmit={searchHandler} >
+            <label htmlFor="name">Name:</label>
+            <input name="name" id="name" onChange={({ target }) => setName(target.value)} value={name} />
+            <label htmlFor="email">Email:</label>
+            <input name="email" type="email" id="email" onChange={({ target }) => setEmail(target.value)} value={email} />
+            <input type="submit" value="Search" />
+        </form>
     )
 }
 
